c35dddfa7455b2dfa11396e443f109f010fcb2cf
Omri <omri.ronen4@gmail.com>
2021-04-19 12:37:46 -0700

    made the time stamps padding match the style of the shorter length times

--- script.js
+++ script.js
@@ -8,27 +8,20 @@ var textArea = $(".form-control"); // text area where user can enter daily plans
 var presentDate = today.format('MMMM Do YYYY'); // this holds the present date 
 
 timeOfDayArray24 = ['00:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00',]
-timeOfDayArray12 = ['12:00 AM','1:00 AM','2:00 AM','3:00 AM','4:00 AM','5:00 AM','6:00 AM','7:00 AM','8:00 AM','9:00 AM','10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM','5:00 PM','6:00 PM','7:00 PM','8:00 PM','9:00 PM','10:00 PM','11:00 PM',]
+timeOfDayArray12 = ['12 AM','1 AM','2 AM','3 AM','4 AM','5 AM','6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM','9 PM','10 PM','11 PM',]
 
 
- function changeTimeStandard () {                   //attempted 
-    $(".timeChange").on("click", function(){
-        if (timeOfDayArray12) {
-            timeOfDayArray24;
-        } else if (timeOfDayArray24) {
-            timeOfDayArray12
-        }else {
-            timeOfDayArray12
-        }
-        
-    })
+
+
+
+for(i=0;i<timeOfDayArray12.length;i++) {    
+   hourStamp[i].textContent = timeOfDayArray12[i];
 }
 
 
 
-for(i=0;i<timeOfDayArray12.length;i++) {    //only shows a single hour of the array
-   hourStamp[i].textContent = timeOfDayArray12[i]
-}
+
+
  
 function timeMatch(){
     var currentTime = moment().hour();
